a87b2f11938f0279b5c1c9928f7bbbd6d8b0277e
Marlon Campo <barriguibela@gmail.com>
2023-10-09 17:29:49 -0500

    montando la aplicacion de presupuesto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    IngresoComponent,
+    EgresoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
